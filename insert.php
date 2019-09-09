674f565aa5ccaaa424f8ce3a39ad428a5e504dc6
imamsodikin <55074866+imamsodikin@users.noreply.github.com>
2019-09-09 22:11:22 +0700

    insert.php
    form insert

---
+++ insert.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table>
+<form action="post.php" method="POST">
+    <tr>
+        <td><label for="Nama">Nama</label></td>
+        <td><input type="text" name="nama" id="nama"></td>
+    </tr>
+    <tr>
+        <td><label for="">Tanggal</label></td>
+        <td><input type="text" name="tanggal" id="tanggal"></td>
+    </tr>
+    <tr>
+        <td><label for="fakultas">Fakultas</label></td>
+        <td><input type="text" name="fakultas" id="fakultas"></td>
+    </tr>
+    <tr>
+        <td><input type="submit" value="submit"></td>
+        <td><input type="button" value="reset"></td>
+    </tr>
+</form>    
+</table>
+</body>
